b3b4274fa42112076f56572f133332e42f550637
Ozan Kasikci <kasikciozan@yandex.com>
2018-03-15 10:32:50 +0300

    add activity manager struct

--- telegrambot/activity-manager.go
+++ telegrambot/activity-manager.go
@@ -7,28 +7,36 @@ import (
 	"os"
 )
 
-var client *redis.Client
+var activityManager *ActivityManager
 var redisOnce sync.Once
 
-func GetActivityManager() *redis.Client {
+type ActivityManager struct {
+	redis *redis.Client
+}
+
+func GetActivityManager() *ActivityManager {
 	redisOnce.Do(func() {
 		redisHost := "127.0.0.1"
 		if os.Getenv("REDIS_HOST") != "" {
 			redisHost = os.Getenv("REDIS_HOST")
 		}
 		
-		client = redis.NewClient(&redis.Options{
+		redis := redis.NewClient(&redis.Options{
 			Addr:     redisHost + ":6379",
 			Password: "", // no password set
 			DB:       0,  // use default DB
 		})
+
+		activityManager = &ActivityManager{
+			redis: redis,
+		}
 	})
 
-	_, err := client.Ping().Result()
+	_, err := activityManager.redis.Ping().Result()
 	if err != nil {
 		log.Fatalln("Can not reach redis")
 	}
 
-	return client
+	return activityManager
 }
 
