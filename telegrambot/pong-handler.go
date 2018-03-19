af56e118bb676f6b2109a56898e083c2b783487f
Ozan Kasikci <kasikciozan@yandex.com>
2018-03-19 22:53:41 +0300

    fix work log creation bug

--- telegrambot/pong-handler.go
+++ telegrambot/pong-handler.go
@@ -10,12 +10,17 @@ func NewPongHandler() *Handler {
 	function := func(ctx context.Context, h *Handler, m *tb.Message) {
 		activityManager := GetActivityManager()
 		activityManager.GetUserHashField(m.Sender.ID, "lastPongDate")
-		activityManager.CacheLastPongDate(m.Sender.ID)
-		activityManager.DelUserHashField(m.Sender.ID, "lastPingDate")
 
-		options := &firebase.WorkLogsOptions{UserID: m.Sender.ID}
-		firebase.CreateWorkLog(ctx, options)
-		h.SetResponseMessage("Thanks for being here!")
+		if activityManager.GetUserHashField(m.Sender.ID, "lastPingDate") != "" {
+			options := &firebase.WorkLogsOptions{UserID: m.Sender.ID}
+			firebase.CreateWorkLog(ctx, options)
+			activityManager.CacheLastPongDate(m.Sender.ID)
+			activityManager.DelUserHashField(m.Sender.ID, "lastPingDate")
+			h.SetResponseMessage("Thanks for being here. A work log has been created!")
+		} else {
+			h.SetResponseMessage("Thanks but you were not pinged yet!")
+		}
+
 	}
 
 	return &Handler{
